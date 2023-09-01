40365047d2c2506b8e7deaaab7389944191fe2d2
Kseniya Shakurova <6275751@mail.ru>
2023-09-01 11:57:29 +0400

    137

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes');
 const { URL_REGEX } = require('./utils/constant');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(cors({ origin: 'https://api.shakurovak.nomoredomainsicu.ru', credentials: true }));
