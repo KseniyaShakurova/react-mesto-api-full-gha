d84f077bd6f5234e4f019c8e7430a5d671e3ae9b
Kseniya Shakurova <6275751@mail.ru>
2023-09-01 11:53:44 +0400

    135

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,8 +1,6 @@
 export default class Api {
   constructor(options) {
     this._baseUrl = options.baseUrl;
-    this._headers = options.headers;
-    this._token = options.token;
   }
 
   _handleResponse(res) {
@@ -12,67 +10,77 @@ export default class Api {
     return res.json();
   }
 
-  getUserInfo() {
+  getUserInfo(token) {
     return fetch(`${this._baseUrl}/users/me`, {
       headers:  {
-        authorization: `Bearer ${this._token}`
+        Authorization : `Bearer ${token}`
       },
     }).then(this._handleResponse);
   }
 
-  getInitialCards() {
+  getInitialCards(token) {
     return fetch(`${this._baseUrl}/cards`, {
-      method: "GET",
-      headers: this._headers,
+      headers: {
+        Authorization : `Bearer ${token}`
+      },
     }).then(this._handleResponse);
   }
 
-  setUserInfo(data) {
+  setUserInfo(data, token) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
-      headers: this._headers,
+      headers: {
+        'Content-Type' : "application/json",
+        Authorization : `Bearer ${token}`
+      },
       body: JSON.stringify(data),
     }).then(this._handleResponse);
   }
 
-  createNewCard(data) {
+  createNewCard(data, token) {
     return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
-      headers:  {
-        authorization: `Bearer ${this._token}`
+      headers: {
+        'Content-Type' : "application/json",
+        Authorization : `Bearer ${token}`
       },
       body: JSON.stringify(data),
     }).then(this._handleResponse);
   }
 
-  likeCard(data) {
+  likeCard(data, token) {
     return fetch(`${this._baseUrl}/cards/${data}/likes`, {
       method: "PUT",
-      headers: this._token,
+      headers: {
+        Authorization : `Bearer ${token}`
+    },
     }).then(this._handleResponse);
   }
 
-  disLike(data) {
+  disLike(data, token) {
     return fetch(`${this._baseUrl}/cards/${data}/likes`, {
       method: "DELETE",
-      headers: this._token,
+      headers: {
+        Authorization : `Bearer ${token}`
+    },
     }).then(this._handleResponse);
   }
 
-  deleteCard(data) {
+  deleteCard(data, token) {
     return fetch(`${this._baseUrl}/cards/${data}`, {
       method: "DELETE",
-      headers:  {
-        authorization: `Bearer ${this._token}`
-      },
+      headers: {
+        Authorization : `Bearer ${token}`
+    },
     }).then(this._handleResponse);
   }
 
   updateAvatar(data) {
     return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
-      headers:  {
-        authorization: `Bearer ${this._token}`
+      headers: {
+        'Content-Type' : "application/json",
+            Authorization : `Bearer ${token}`
       },
       body: JSON.stringify(data),
     }).then(this._handleResponse);
@@ -81,10 +89,6 @@ export default class Api {
 }
 const api = new Api({
   baseUrl: "https://api.shakurovak.nomoredomainsicu.ru",
-  headers: {
-    'Content-Type': 'application/json',
-    
-  },
 });
 
 export { api };
